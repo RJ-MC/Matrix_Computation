9537c7606e3e3febae5d37987d3daf1397531234
RJ-MC <32288077+RJ-MC@users.noreply.github.com>
2020-06-12 22:39:52 -0400

    Update matrix.h

--- includes/matrix.h
+++ includes/matrix.h
@@ -18,48 +18,28 @@ public:
 	matrix(const matrix<_T> &);
 	~matrix();
 	
+	_T* operator[] (int _row);
 	const _T get(int) const;
 	const _T get(int, int) const;
+	matrix<_T>& get(const matrix<int>&) const;
+	matrix<_T>& get(const matrix<int>&,const matrix<int>&) const;
+	matrix<_T>& operator[] (const matrix<int>&) const;
+	
+	template <class _U>
+	matrix<_T>& set(const matrix<int>&, const matrix<_U>&);
 	
 	matrix T(bool _conj = true) const;
-	_T* operator[] (int _row);
-	
-	matrix<_T>& operator[] (const matrix<int>&);
-	
-	int numel() const;
-	
-	/*;
-	matrix utri(int = 0);
-	matrix ltri(int = 0);
-	*/
-	
-	matrix<_T>& reshape(const matrix<int>&) const;
 	
 	template <class _U>
 	operator matrix<_U>() const;
 	
 	matrix<_T>& operator=(const matrix<_T>& );
-	
 	template <class _U>
 	matrix<_T>& operator=(const matrix<_U>& );
 	
-	template <class _U>
-	void set(const matrix<int>&, const matrix<_U>&);
-	
-	template<class _U, class _V>
-	friend bool operator == (const matrix<_U>&, const matrix<_V>&);
-	template<class _U, class _V>
-	friend bool operator != (const matrix<_U>&, const matrix<_V>&);
-	
-	template <class _U>
-	friend ostream& operator << (ostream &, const matrix<_U>&);
-
-	matrix<_T>& submtr(const matrix<int>&, const matrix<int>&);
-
 };
 
 //What happens to me here?
 #include "matrix.cpp"
 
 #endif
-
